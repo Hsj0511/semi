355a00e6b1d3b425b67affd79cc160d22f5bb9e5
Hsj0511 <0511hsj@gmail.com>
2023-02-21 01:34:47 +0900

    review page
    review page ㅈㅏㄱㅇㅓㅂㅈㅜㅇ

--- src/board/DAO/BoardDAO.java
+++ src/board/DAO/BoardDAO.java
@@ -2,16 +2,44 @@ package board.DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import board.VO.BoardVO;
 import static movie.dao.DbDAO.*;
 
 public class BoardDAO {
-
-	public int insertReview(Connection conn, BoardVO vo) {
-		int result = -1;
+	private Connection conn;
+	public BoardDAO(Connection conn) {
+		this.conn=conn;
+	};
+	
+	public int getIndex() {
+		int maxNum =0;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 		
-		String sql = " insert into review1 ?, ?, ?, ? ";
+		
+		
+		try {
+			String sql = " select nvl(max(num),0) from review1 ";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				maxNum = rs.getInt(1);
+			}
+			rs.close();
+			pstmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return maxNum;
+	}
+
+	public int insertReview( BoardVO vo) {
+		int result = 0;
+		
+		String sql = " insert into review1 (num, name, content, postdate) "
+		            + "values(?,?,?, sysdate)";
 		
 		
 		PreparedStatement pstmt = null;
@@ -20,18 +48,19 @@ public class BoardDAO {
 		try {
 			pstmt = conn.prepareStatement(sql);
 			
-			pstmt.setInt(1, vo.getBoardNum());
-			pstmt.setString(2, vo.getBoardWriter());
-			pstmt.setString(3, vo.getBoardContent());
-			pstmt.setDate(4, vo.getBoardDate());
+			pstmt.setInt(1, vo.getNum());
+			pstmt.setString(2, vo.getName());
+			pstmt.setString(3, vo.getContent());
 		
-		result = pstmt.executeUpdate();
+		
+			result = pstmt.executeUpdate();
+			pstmt.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}finally {
 			close(pstmt);
 		}
-		System.out.println("dao return:  " + result);
+		System.out.println("디비 입력 성공");
 		return result;
 		
 	}
--- src/board/controller/BoardController.java
+++ src/board/controller/BoardController.java
@@ -1,16 +1,27 @@
 package board.controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import board.VO.BoardVO;
+import board.service.BoardService;
+
 /**
  * Servlet implementation class BoardController
  */
-@WebServlet("/reviewPage")
+@WebServlet("/reviewPage1")
 public class BoardController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -36,18 +47,46 @@ public class BoardController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("영화1 리뷰 포스트");
 		
+
 		
-		String boardNum = request.getParameter("boardNum");
+		
 		String boardWriter = request.getParameter("boardWriter");
 		String boardContent = request.getParameter("boardContent");
-		String boardDate =  request.getParameter("boardDate");
+		Timestamp boardDate = new Timestamp(System.currentTimeMillis());
+		BoardVO vo = new BoardVO();
+		System.out.println(boardDate);
 
-		response.getWriter().append(boardNum);
-		response.getWriter().append(boardWriter);
-		response.getWriter().append(boardContent);
-		response.getWriter().append(boardDate);
+
+	
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			Connection conn=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","MOVIE","MOVIE");
+			System.out.println("리뷰 디비 접속 성공!");
+			
+			
+			vo.setBoardWriter(boardWriter);
+			vo.setBoardContent(boardContent);
+		//	vo.setBoardDate(boardDate);
 		
-		System.out.println(boardWriter);
+
+			
+			Statement stmt = conn.createStatement();
+			String sql =" insert into review1 (boardSeq, boardWriter, boardContent, boardDate)" +
+			           " values (review1_seq.nextval, '"+boardWriter+"','"+boardContent+"','"+boardDate+"') ";
+			
+			stmt.executeUpdate(sql);
+			
+			
+			System.out.println("리뷰 insert 성공");
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("리뷰 디비 접속 실패");
+    	} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+	   } finally {
+		   response.sendRedirect(request.getContextPath()+ "/reviewPage1");
+	   }
 	}
 
 }
