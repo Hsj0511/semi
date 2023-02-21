8b359fcc504fa8de6f3a44dbc94febf3effeef8b
Hsj0511 <0511hsj@gmail.com>
2023-02-21 14:41:45 +0900

    변경
    변경

--- src/board/controller/BoardController.java
+++ src/board/controller/BoardController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class BoardController
  */
-@WebServlet("/reviewPage")
+@WebServlet("/reviewPage1")
 public class BoardController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
--- src/board/service/BoardService.java
+++ src/board/service/BoardService.java
@@ -8,12 +8,5 @@ import static movie.dao.DbDAO.*;
 
 public class BoardService {
 
-	public int insertReview(BoardVO vo) {
-		int result = 0;
-		Connection conn = getConnection();
-		result = new BoardDAO().insertReview(conn, vo);
-		close(conn);
-		return result;
-	}
 
 }
